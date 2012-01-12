c4ea6e264a49bd231ebb6b69b31b7e8059948875
Kent Nguyen <nguyen.dmz@gmail.com>
2012-01-13 01:16:30 +0800

    Clean up the tabs

--- KNPathTableViewControllerDemo/KNAppDelegate.m
+++ KNPathTableViewControllerDemo/KNAppDelegate.m
@@ -18,10 +18,19 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
   self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
 
+  // First tab
   KNFirstViewController *viewController1 = [[KNFirstViewController alloc] initWithStyle:UITableViewStylePlain];
+  UINavigationController * uinav1 = [[UINavigationController alloc] initWithRootViewController:viewController1];
+
+  
+  
+  // About tab
   UIViewController *viewController2 = [[KNSecondViewController alloc] initWithNibName:@"KNSecondViewController" bundle:nil];
+  UINavigationController * uinav2 = [[UINavigationController alloc] initWithRootViewController:viewController2];
+
+  // The tab bar
   self.tabBarController = [[UITabBarController alloc] init];
-  self.tabBarController.viewControllers = [NSArray arrayWithObjects:viewController1, viewController2, nil];
+  self.tabBarController.viewControllers = [NSArray arrayWithObjects:uinav1, uinav2, nil];
   self.window.rootViewController = self.tabBarController;
   [self.window makeKeyAndVisible];
   return YES;
