5583df4af8db04bd922033cbe1da47c763d086d8
Kent Nguyen <nguyen.dmz@gmail.com>
2012-01-12 21:43:06 +0800

    Create custom class

---
+++ KNPathTableViewController/KNPathTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  KNPathTableViewController.h
+//  
+//
+//  Created by Kent Nguyen 12/1/12.
+//  Copyright (c) 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KNPathTableViewController : UITableViewController
+
+@end
---
+++ KNPathTableViewController/KNPathTableViewController.m
@@ -0,0 +1,45 @@
+//
+//  KNPathTableViewController.m
+//  
+//
+//  Created by Kent Nguyen 12/1/12.
+//  Copyright (c) 2012. All rights reserved.
+//
+
+#import "KNPathTableViewController.h"
+
+
+@implementation KNPathTableViewController
+
+
+- (void)viewDidLoad {
+  [super viewDidLoad];
+}
+
+#pragma mark - Blank implementations
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+  return 0;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+  return 0;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+  static NSString *CellIdentifier = @"Cell";
+
+  UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+  if (cell == nil) {
+      cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier];
+  }
+
+  return cell;
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+}
+
+#pragma mark - Used to be dealloc here
+
+@end
